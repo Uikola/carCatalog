88daf3ce4cf4b33e352de233d9648753a0ac7075
uikola <ugulaev806@yandex.com>
2024-04-30 09:03:00 +0400

    Add logs to the database initialization function

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,12 +2,15 @@ package db
 
 import (
 	"github.com/jmoiron/sqlx"
+	"github.com/rs/zerolog/log"
 )
 
 func InitDB(pgURL string) (*sqlx.DB, error) {
+	log.Info().Msg("Database initializing")
 	db, err := sqlx.Connect("pgx", pgURL)
 	if err != nil {
 		return nil, err
 	}
+	log.Info().Msg("database initialized successfully")
 	return db, nil
 }
